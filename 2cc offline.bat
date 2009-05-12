6d78ebe7404953fb6df44ca330b0d56e669dd1d5
DJ Nekkid <DJ Nekkid@localhost>
2009-05-12 13:14:55 +0200

    a small update

--- 2cc offline.bat
+++ 2cc offline.bat
@@ -1,7 +1,7 @@
   @ECHO OFF
 ECHO.
 Echo Compiling files to one .nfo
-cd "Y:\grf\grf\2cc\sprites"
+cd sprites
 copy /Y nfo\2africa\a0a4.nfo+nfo\2africa\engine.nfo+nfo\2africa\mus.nfo+nfo\2africa\metro.nfo nfo\2.nfo
 copy /Y nfo\3asia\a0a4.nfo+nfo\3asia\engine.nfo+nfo\3asia\mus.nfo+nfo\3asia\metro.nfo nfo\3.nfo
 copy /Y nfo\4e-eu\a0a4.nfo+nfo\4e-eu\engine.nfo+nfo\4e-eu\mus.nfo+nfo\4e-eu\metro.nfo nfo\4.nfo
@@ -12,7 +12,7 @@ copy /Y nfo\8scandinavia\a0a4.nfo+nfo\8scandinavia\engine.nfo+nfo\8scandinavia\m
 copy /Y nfo\9s-eu\a0a4.nfo+nfo\9s-eu\engine.nfo+nfo\9s-eu\mus.nfo+nfo\9s-eu\metro.nfo nfo\9.nfo
 copy /Y nfo\10w-eu\a0a4.nfo+nfo\10w-eu\engine.nfo+nfo\10w-eu\mus.nfo+nfo\10w-eu\metro.nfo nfo\10.nfo
 Echo on
-copy /Y nfo\header.nfo+nfo\wagons.nfo+nfo\2.nfo+nfo\3.nfo+nfo\4.nfo+nfo\5.nfo+nfo\6.nfo+nfo\7.nfo+nfo\8.nfo+nfo\9.nfo+nfo\10.nfo+nfo\metrorail.nfo+nfo\regsel.nfo "Y:\grf\grf\2cc\sprites\2ccdj.nfo"
+copy /Y nfo\header.nfo+nfo\wagons.nfo+nfo\2.nfo+nfo\3.nfo+nfo\4.nfo+nfo\5.nfo+nfo\6.nfo+nfo\7.nfo+nfo\8.nfo+nfo\9.nfo+nfo\10.nfo+nfo\metrorail.nfo+nfo\regsel.nfo 2ccdj.nfo
 
 cd ..
 ECHO Running NFORenum. . .
