59b44797d47962a503738be7126ec9b3cc649351
DJ Nekkid <DJ Nekkid@localhost>
2009-05-13 18:58:45 +0200

    more sorting, and updated the batchfile with *.nfo

--- 2cc offline.bat
+++ 2cc offline.bat
@@ -2,18 +2,18 @@
 ECHO.
 Echo Compiling files to one .nfo
 cd sprites
-copy /Y nfo\2africa\a0a4.nfo+nfo\2africa\engine.nfo+nfo\2africa\mus.nfo+nfo\2africa\metro.nfo nfo\2.nfo
-copy /Y nfo\3asia\a0a4.nfo+nfo\3asia\engine.nfo+nfo\3asia\mus.nfo+nfo\3asia\metro.nfo nfo\3.nfo
-copy /Y nfo\4e-eu\a0a4.nfo+nfo\4e-eu\engine.nfo+nfo\4e-eu\mus.nfo+nfo\4e-eu\metro.nfo nfo\4.nfo
-copy /Y nfo\5n-am\a0a4.nfo+nfo\5n-am\engine.nfo+nfo\5n-am\mus.nfo+nfo\5n-am\metro.nfo nfo\5.nfo
-copy /Y nfo\6s-am\a0a4.nfo+nfo\6s-am\engine.nfo+nfo\6s-am\mus.nfo+nfo\6s-am\metro.nfo nfo\6.nfo
-copy /Y nfo\7ocean\a0a4.nfo+nfo\7ocean\engine.nfo+nfo\7ocean\mus.nfo+nfo\7ocean\metro.nfo nfo\7.nfo
-copy /Y nfo\8scandinavia\a0a4.nfo+nfo\8scandinavia\engine.nfo+nfo\8scandinavia\mus.nfo+nfo\8scandinavia\metro.nfo nfo\8.nfo
-copy /Y nfo\9s-eu\a0a4.nfo+nfo\9s-eu\engine.nfo+nfo\9s-eu\mus.nfo+nfo\9s-eu\metro.nfo nfo\9.nfo
-copy /Y nfo\10w-eu\a0a4.nfo+nfo\10w-eu\engine.nfo+nfo\10w-eu\mus.nfo+nfo\10w-eu\metro.nfo nfo\10.nfo
+copy /Y nfo\2africa\*.nfo nfo\02.nfo
+copy /Y nfo\3asia\*.nfo nfo\03.nfo
+copy /Y nfo\4e-eu\*.nfo nfo\04.nfo
+copy /Y nfo\5n-am\*.nfo nfo\05.nfo
+copy /Y nfo\6s-am\*.nfo nfo\06.nfo
+copy /Y nfo\7ocean\*.nfo nfo\07.nfo
+copy /Y nfo\8scandinavia\*.nfo nfo\08.nfo
+copy /Y nfo\9s-eu\*.nfo nfo\09.nfo
+copy /Y nfo\10w-eu\*.nfo nfo\10.nfo
 Echo on
-copy /Y nfo\0header.nfo+nfo\1wagons.nfo+nfo\2.nfo+nfo\3.nfo+nfo\4.nfo+nfo\5.nfo+nfo\6.nfo+nfo\7.nfo+nfo\8.nfo+nfo\9.nfo+nfo\10.nfo+nfo\metrorail.nfo+nfo\regsel.nfo 2ccdj.nfo
-
+copy /Y nfo\*.nfo 2ccdj.nfo
+pause
 cd ..
 ECHO Running NFORenum. . .
 @ECHO ON
