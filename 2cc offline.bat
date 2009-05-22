3cd6740b3fc8fc2f06926c298f9b6197d2b22560
DJ Nekkid <DJ Nekkid@localhost>
2009-05-22 22:07:40 +0200

    fixed even more in the .bat file

--- 2cc offline.bat
+++ 2cc offline.bat
@@ -16,27 +16,24 @@ Echo on
 copy /Y nfo\*.nfo 2cc_trainset.nfo.pre
 pause
 
-
 ECHO sed...
+
 "C:\Program Files\GnuWin32\bin\sed.exe" -f ..\scripts\nfo.sed 2cc_trainset.nfo.pre > 2cc_trainset.nfo
 del 2cc_trainset.nfo.pre
 pause
 cd ..
 ECHO Running NFORenum. . .
-@ECHO ON
+
 renum.exe -k -w 42,94,141,143,144,147,17 2cc_trainset.nfo
-echo :) -w 42 -w 94 -w 141 -w 143 -w 144 -w 147 -w 170
-@ECHO OFF
+
 pause
-ECHO.
-ECHO Running GRFCodec. . .
-@ECHO ON
-grfcodec.exe -e -p 2 2cc_trainset.nfo.new.nfo
-@ECHO OFF
-ECHO.
-ECHO Remove old file, rename new file and copy file to ottd and ttdp dirs. . .
-DEL .\2cc_trainset.grf
-ren 2cc_trainset.nfo.new.new.new 2cc_trainset.grf
+
+ren sprites\2cc_trainset.nfo.new.nfo 2cc.nfo
+del 2cc.grf
+grfcodec.exe -e -p 2 2cc.nfo
+
+
+ren 2cc.grf 2cc_trainset.grf
 COPY .\2cc_trainset.grf "C:\data\OpenTTD\data" /Y
 
 ECHO Done!
